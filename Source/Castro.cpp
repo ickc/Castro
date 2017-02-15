215db12698bc1b4628164c5e067a0fa366d5555c
Max Katz <maximilian.katz@stonybrook.edu>
2017-02-15 01:30:19 -0500

    Correct the deferred gravitational sync strategy
    The correct strategy needs to update the potential temporarily before
    recomputing the source terms on the coarse level. However, it cannot leave
    the change there, as it defeats the purpose of the deferred sync (which
    is to store the change separately, and add it back when the coarse grid
    potential is linearly interpolated in time to create boundary conditions on
    the fine grid -- storing it in the full state makes the time interpolation
    wrong). So after we recompute the new-time source terms, we subtract off
    the sync from the final time state (unless we've reached the end of the
    timestep and thus the synchronization time between the coarse and fine
    levels, in which case we leave it there as we no longer need to defer
    the update).

--- Source/Castro.cpp
+++ Source/Castro.cpp
@@ -1550,21 +1550,6 @@ Castro::post_timestep (int iteration)
 	    reflux(level, level+1);
     }
 
-    // Add the deferred delta_phi contribution to the state now that we've completed the timestep.
-
-#ifdef SELF_GRAVITY
-    if (do_grav && gravity->get_gravity_type() == "PoissonGrav" && gravity->NoSync() == 0) {
-
-      MultiFab::Add(get_new_data(PhiGrav_Type), *(gravity->get_delta_phi(level)), 0, 0, 1, 1);
-
-      for (int n = 0; n < BL_SPACEDIM; ++n)
-	gravity->plus_grad_phi_curr(level, gravity->get_grad_delta_phi(level));
-
-      gravity->get_new_grav_vector(level, get_new_data(Gravity_Type), get_state_data(Gravity_Type).curTime());
-
-    }
-#endif
-
     // Ensure consistency with finer grids.
 
     if (level < finest_level)
@@ -2077,6 +2062,10 @@ Castro::reflux(int crse_level, int fine_level)
 
     const Real strt = ParallelDescriptor::second();
 
+    // Create temporary MultiFabs to store the contributions from
+    // refluxing and the coarse-fine mismatch in the potential that
+    // are used in the gravitational sync solve.
+
 #ifdef SELF_GRAVITY
     int nlevs = fine_level - crse_level + 1;
 
@@ -2118,7 +2107,8 @@ Castro::reflux(int crse_level, int fine_level)
 
 	reg->Reflux(state, crse_lev.volume, 1.0, 0, 0, NUM_STATE, crse_lev.geom);
 
-	// Store the density change, for the gravity sync.
+	// Store the contribution to the gravitational sync solve from the change in
+	// the density due to hydrodynamic refluxing.
 
 #ifdef SELF_GRAVITY
 	int ilev = lev - crse_level - 1;
@@ -2246,22 +2236,17 @@ Castro::reflux(int crse_level, int fine_level)
 
 	    reg = &getLevel(lev).phi_reg;
 
-	    // Note that the scaling by the area here is corrected for by dividing by the
-	    // cell volume in the reflux. In this way we get a discrete divergence that
-	    // is analogous to the divergence of the flux in the hydrodynamics. See Equation
-	    // 37 in the Castro I paper. The dimensions of dphi are therefore actually
-	    // phi / cm**2, which makes it correct for the RHS of the Poisson equation.
-
-	    // It is important to understand that we have to be careful about what we're actually
-	    // adding to this flux register. For reflux_strategy == 2, we can just add the fine grid
-	    // grad(phi) and subtract the coarse grid grad(phi) as the source to the sync solve.
-	    // This is because they both exist at the same simulation time so it's fair to compare them.
-	    // But if we're doing reflux_strategy == 1, the coarse grid and fine grid are not yet at
-	    // the same simulation time when we call this sync, so we have to interpolate backwards
-	    // in time to figure out what the effective coarse grid potential would be. Then we can
-	    // use that as the sync source. Afterward, we have to save the resulting delta_phi and
-	    // delta_grad_phi and only add them back when we're computing the interpolated coarse
-	    // grid potential in the next fine timestep.
+	    // Compute the contribution to the gravitational sync solve from the mismatch between
+	    // grad(phi) on the coarse and fine levels. For reflux_strategy == 2, the "old" method,
+	    // we can just add the fine grid grad(phi) and subtract the coarse grid grad(phi) as the
+	    // source to the sync solve. This is because they both exist at the same simulation time
+	    // so it's fair to compare them. If instead we're doing the new method, reflux_strategy
+	    // == 1, the coarse grid and fine grid are not yet at the same simulation time when we
+	    // call the sync solve, so we have to interpolate backwards in time to figure out what
+	    // the effective coarse grid potential would be. Then we can use that as the sync source.
+	    // Afterward, we have to save the resulting delta_phi and grad_delta_phi and only add
+	    // them back when we're computing the interpolated coarse grid potential in the next
+	    // fine timestep.
 
 	    for (int i = 0; i < BL_SPACEDIM; ++i) {
 
@@ -2278,12 +2263,22 @@ Castro::reflux(int crse_level, int fine_level)
 				  omalpha, gravity->get_grad_phi_prev(lev-1)[i], 0,
 				  0, 1, 0);
 
+
+		// Note that the scaling by the area here is corrected for by dividing by the
+		// cell volume in the reflux. In this way we get a discrete divergence that
+		// is analogous to the divergence of the flux in the hydrodynamics. See Equation
+		// 37 in the Castro I paper. The dimensions of dphi are therefore actually
+		// phi / cm**2, which makes it correct for the RHS of the Poisson equation.
+
 		reg->CrseInit(crse_grad_phi, crse_lev.area[i], i, 0, 0, 1, -1.0);
 		reg->FineAdd(gravity->get_grad_phi_curr(lev)[i], fine_lev.area[i], i, 0, 0, 1, 1.0);
 
 	    }
 
 	    reg->Reflux(dphi[ilev], crse_lev.volume, 1.0, 0, 0, 1, crse_lev.geom);
+
+	    // Average down so that the contribution to the upcoming multi-level Poisson solve
+	    // has consistent data across levels.
 
 	    BoxLib::average_down(dphi[ilev + 1], dphi[ilev], 0, 1, getLevel(lev).crse_ratio);
 
@@ -2297,8 +2292,29 @@ Castro::reflux(int crse_level, int fine_level)
     // Do the sync solve across all levels.
 
 #ifdef SELF_GRAVITY
-    if (do_grav && gravity->get_gravity_type() == "PoissonGrav" && gravity->NoSync() == 0)
-	    gravity->gravity_sync(crse_level, fine_level, drho, dphi);
+    if (do_grav && gravity->get_gravity_type() == "PoissonGrav" && gravity->NoSync() == 0) {
+
+	gravity->gravity_sync(crse_level, fine_level, drho, dphi);
+
+        // Add the deferred delta_phi contribution temporarily for the purposes of calculating
+	// the updated new-time source terms on the coarse level.
+
+	for (int lev = fine_level; lev >= crse_level; --lev) {
+
+	    // Add the change in potential and change in edge-centered gravitational acceleration.
+
+	    getLevel(lev).get_new_data(PhiGrav_Type).plus(*(gravity->get_delta_phi(lev)), 0, 1, 1);
+
+	    for (int n = 0; n < BL_SPACEDIM; ++n)
+		gravity->get_grad_phi_curr(lev)[n].plus(gravity->get_grad_delta_phi(lev)[n], 0, 1, 0);
+
+	    // Recalculate the cell-centered gravity from the edge-centered gravity.
+
+	    gravity->get_new_grav_vector(lev, getLevel(lev).get_new_data(Gravity_Type), getLevel(lev).get_state_data(Gravity_Type).curTime());
+
+	}
+
+    }
 #endif
 
     // Now subtract the new-time updates to the state data,
@@ -2334,6 +2350,64 @@ Castro::reflux(int crse_level, int fine_level)
 	}
 
     }
+
+    // Now that we have recomputed the source terms, we need to fix
+    // the gravitational field. If we are on an intermediate timestep
+    // on a level below the finest level, we are employing a deferred
+    // sync strategy where we store the sync solve contribution in a
+    // temporary array that is called upon when we compute the boundary
+    // conditions on the level solve for the level above this one, so
+    // we do not actually want to keep the contribution from delta_phi
+    // in the state. If, however, we're on the finest level, or we have
+    // completed all fine timesteps on the level above this one, then
+    // we can keep the contribution from delta_phi in the full potential,
+    // as we no longer need to defer the update.
+
+#ifdef SELF_GRAVITY
+    if (do_grav && gravity->get_gravity_type() == "PoissonGrav" && gravity->NoSync() == 0) {
+
+	for (int lev = fine_level; lev >= crse_level; --lev) {
+
+	    bool subtract_deferred_sync = true;
+
+	    if (lev == parent->finestLevel()) {
+		subtract_deferred_sync = false;
+	    } else {
+		if (getLevel(lev+1).iteration == parent->nCycle(lev+1)) {
+		    subtract_deferred_sync = false;
+		}
+	    }
+
+	    if (subtract_deferred_sync) {
+
+		// Subtract the change in potential and change in (edge-centered) gravitational acceleration.
+
+		getLevel(lev).get_new_data(PhiGrav_Type).minus(*(gravity->get_delta_phi(lev)), 0, 1, 1);
+
+		for (int n = 0; n < BL_SPACEDIM; ++n)
+		    gravity->get_grad_phi_curr(lev)[n].minus(gravity->get_grad_delta_phi(lev)[n], 0, 1, 0);
+
+		// Recalculate the cell-centered gravity from the edge-centered gravity.
+
+		gravity->get_new_grav_vector(lev, getLevel(lev).get_new_data(Gravity_Type), getLevel(lev).get_state_data(Gravity_Type).curTime());
+
+	    } else {
+
+		// If we get here, it means we're keeping the change to the state.
+		// We no longer have a need for the deferred sync. Zero it out
+		// so that it doesn't interfere with future timesteps.
+
+		gravity->get_delta_phi(lev)->setVal(0.0);
+
+		for (int n = 0; n < BL_SPACEDIM; ++n)
+		    gravity->get_grad_delta_phi(lev)[n].setVal(0.0);
+
+	    }
+
+	}
+
+    }
+#endif
 
     if (verbose)
     {
