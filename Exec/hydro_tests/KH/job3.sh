3ca71997aa4f6eee09c92730b427f9981181157e
Kolen Cheung <christian.kolen@gmail.com>
2017-04-09 18:40:31 -0700

    add Exec/hydro_tests/KH/job3.sh

---
+++ Exec/hydro_tests/KH/job3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -C haswell
+#SBATCH -p debug
+#SBATCH -J Castro-KH-problem-3
+#SBATCH -t 00:10:00
+
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+export OMP_PLACES=threads
+export OMP_PROC_BIND=spread
+
+
+#run the application:
+srun -n 32 -c 2 --cpu_bind=cores ./Castro2d.gnu.MPI.ex inputs.2d
